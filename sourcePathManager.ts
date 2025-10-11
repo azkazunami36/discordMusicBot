aad59c0ee4e0ad637c32fb46654a095f4073bfa7
azkazunami36 <kazunami36.sum@icloud.com>
2025-10-12 00:21:37 +0900

    臨時でcookie.txtを設置

--- sourcePathManager.ts
+++ sourcePathManager.ts
@@ -61,13 +61,18 @@ export class SourcePathManager {
                         resolution: string;
                     }
                     const formats: Format[] = await new Promise((resolve, reject) => {
-                        exec('yt-dlp --print "%(formats)j" -q --extractor-args youtube:player_client=tv_embedded --cookies-from-browser chrome --no-warnings https://youtu.be/' + videoId, (err, stdout, stderr) => {
-                            try { resolve(JSON.parse(stdout)) }
-                            catch (e) {
-                                console.log(videoId);
-                                reject(e);
-                            }
+                        let text = "";
+                        const proc = exec('yt-dlp --print "%(formats)j" -q --extractor-args youtube:player_client=tv_embedded --cookies /Users/kazunami36_sum2/cookies.txt --no-warnings https://youtu.be/' + videoId, (err, stdout, stderr) => {
+                            text += stdout;
                         });
+                        proc.on("close", () => {
+                            try { resolve(JSON.parse(text)) }
+                            catch (e) {
+                                console.log("次の動画のJSONをパースしようとしたらエラーが発生しました。", videoId, text);
+                                reject(new Error("この動画(" + videoId + ")の解析を行うためにフォーマットリストを解析しようとした時にエラーが発生しました。: " + e));
+                            }
+                        })
+
                     });
                     function pickBestFormat(formats: Format[]): Format | undefined {
                         return formats
@@ -96,7 +101,7 @@ export class SourcePathManager {
                                 "-o", "youtubeCache/%(id)s-cache.%(ext)s",
                                 "--progress-template", "%(progress)j",
                                 "--extractor-args", 'youtube:player_client=tv_embedded',
-                                "--cookies-from-browser", "chrome",
+                                "--cookies", "/Users/kazunami36_sum2/cookies.txt",
                                 `https://youtu.be/${videoId}`
                             ], { cwd: process.cwd() });
 
@@ -201,7 +206,7 @@ export class SourcePathManager {
                         tbr?: number | null;  // total bitrate (kbps)
                     }
                     const formats: Format[] = await new Promise((resolve, reject) => {
-                        exec('yt-dlp --print "%(formats)j" -q --cookies-from-browser chrome --no-warnings --add-header "Referer:https://www.nicovideo.jp/" https://www.nicovideo.jp/watch/' + nicovideoId, (err, stdout, stderr) => {
+                        exec('yt-dlp --print "%(formats)j" -q --cookies /Users/kazunami36_sum2/cookies.txt --no-warnings --add-header "Referer:https://www.nicovideo.jp/" https://www.nicovideo.jp/watch/' + nicovideoId, (err, stdout, stderr) => {
                             if (stderr) console.log(stderr);
                             try { resolve(JSON.parse(stdout)) }
                             catch (e) {
@@ -266,7 +271,7 @@ export class SourcePathManager {
                                 "-f", audioformat.format_id,
                                 "-o", "niconicoCache/%(id)s-cache.%(ext)s",
                                 "--progress-template", "%(progress)j",
-                                "--cookies-from-browser", "chrome",
+                                "--cookies", "/Users/kazunami36_sum2/cookies.txt",
                                 "--add-header", "Referer:https://www.nicovideo.jp/",
                                 `https://www.nicovideo.jp/watch/${nicovideoId}`
                             ], { cwd: process.cwd() });
@@ -360,7 +365,7 @@ export class SourcePathManager {
                         resolution: string;
                     }
                     const formats: Format[] = await new Promise((resolve, reject) => {
-                        exec('yt-dlp --print "%(formats)j" -q --cookies-from-browser chrome --no-warnings https://www.x.com/i/web/status/' + twitterId, (err, stdout, stderr) => {
+                        exec('yt-dlp --print "%(formats)j" -q --cookies /Users/kazunami36_sum2/cookies.txt --no-warnings https://www.x.com/i/web/status/' + twitterId, (err, stdout, stderr) => {
                             try { console.log(stdout); resolve(JSON.parse(stdout)) }
                             catch (e) {
                                 console.log(twitterId);
@@ -386,7 +391,7 @@ export class SourcePathManager {
                                 "-f", audioformat.format_id,
                                 "-o", "twitterCache/%(id)s-cache.%(ext)s",
                                 "--progress-template", "%(progress)j",
-                                "--cookies-from-browser", "chrome",
+                                "--cookies", "/Users/kazunami36_sum2/cookies.txt",
                                 "--add-header", "Referer:https://www.nicovideo.jp/",
                                 `https://www.x.com/i/web/status/${twitterId}`
                             ], { cwd: process.cwd() });
