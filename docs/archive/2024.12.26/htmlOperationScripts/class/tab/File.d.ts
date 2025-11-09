974dbbcf2dac1fc425759d61d3ca5e137c5e8472
azkazunami36 <kazunami36.sum@icloud.com>
2025-11-09 21:08:00 +0000

    ほとんどのコードを一度botフォルダに移動

--- docs/archive/2024.12.26/htmlOperationScripts/class/tab/File.d.ts
+++ docs/archive/2024.12.26/htmlOperationScripts/class/tab/File.d.ts
@@ -1,4 +1,4 @@
-import { ActiveElement } from '../../../scripts/class/ActiveElement';
+import { ActiveElement } from '../../../scripts/class/ActiveElement.js';
 import { FileList } from '../panel/List/FileList.js';
 import { PopupManager } from '../popup/popupManager.js';
 export declare class FileTab extends FileList {
--- docs/archive/2024.12.26/htmlOperationScripts/class/tab/Music.d.ts
+++ docs/archive/2024.12.26/htmlOperationScripts/class/tab/Music.d.ts
@@ -1,4 +1,4 @@
-import { ActiveElement } from '../../../scripts/class/ActiveElement';
+import { ActiveElement } from '../../../scripts/class/ActiveElement.js';
 import { MusicList } from '../panel/List/MusicList.js';
 import { PopupManager } from '../popup/popupManager.js';
 export declare class MusicTab extends MusicList {
